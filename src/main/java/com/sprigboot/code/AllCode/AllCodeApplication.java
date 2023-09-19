44c7da4297f12303cf00106e2677940f001c5cc3
Aditya Sinha <adi29195sinha@gmail.com>
2023-09-19 12:35:08 +0530

    code

---
+++ src/main/java/com/sprigboot/code/AllCode/AllCodeApplication.java
@@ -0,0 +1,13 @@
+package com.sprigboot.code.AllCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprigboot/code/AllCode/AllCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprigboot.code.AllCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
